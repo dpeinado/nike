2b9e8217908981bed5713ef01bad4522cec72599
JarrodCTaylor <jarrod.c.taylor@gmail.com>
2015-08-26 10:41:58 +0200

    Segundo commit

---
+++ guardias/tests/test_models.py
@@ -0,0 +1,17 @@
+from test_plus.test import TestCase
+from guardias.models import Organizacion, Centro
+
+class TestCentro(TestCase):
+    def setUp(self):
+        self.organizacion = Organizacion.objects.create(
+            nombre='Hospital Infanta Sofía',
+                                                        )
+
+    def crearCentroSinOrganizacion(self):
+        self.centro1 = Centro.objects.create(
+            nombre = "Servicio de Radiodiagnóstico"
+        )
+        self.assertEqual(
+            self.centro1.__str__(),
+            "Servicio de Radiodiagnóstico"
+        )
